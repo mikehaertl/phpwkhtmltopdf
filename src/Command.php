881943c18ccea9a3f47a0e692d15a565e1152647
Alexey Anisimov <mne@lexa.in>
2015-10-20 17:58:11 +0300

    Fix default options pass to xfvb-run command
    Xvfb failed start error on same port
    
    http://stackoverflow.com/a/25610678/1266649

--- src/Command.php
+++ src/Command.php
@@ -27,9 +27,9 @@ class Command extends BaseCommand
     public $xvfbRunBinary = 'xvfb-run';
 
     /**
-     * @var string options to pass to the xfvb-run command. Default is `--server-args="-screen 0, 1024x768x24"`.
+     * @var string options to pass to the xfvb-run command. Default is `-a --server-args="-screen 0, 1024x768x24"`.
      */
-    public $xvfbRunOptions = '--server-args="-screen 0, 1024x768x24"';
+    public $xvfbRunOptions = '-a --server-args="-screen 0, 1024x768x24"';
 
     /**
      * @param array $args args to add to the command. These can be:
